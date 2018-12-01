c2f30a7f3b0604a5a74242deebe7c89b57fe9fd8
Vivek Das Mohapatra <vivek@collabora.com>
2018-12-01 03:05:45 +0000

    Extraneous extern keywords dropped

--- chainloader/util.h
+++ chainloader/util.h
@@ -5,8 +5,8 @@
 #define EFIDIR       L"EFI"
 #define DEFAULTLDR   L"EFI\\Boot\\bootx64.efi"
 
-extern VOID * efi_alloc (IN UINTN s);
-extern VOID   efi_free  (IN VOID *p);
+VOID * efi_alloc (IN UINTN s);
+VOID   efi_free  (IN VOID *p);
 
 CONST CHAR16 * efi_statstr (EFI_STATUS s);
 
