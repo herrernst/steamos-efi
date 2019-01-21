d0cb88961b4b0aa083ae291a080e04771ee53f59
Vivek Das Mohapatra <vivek@collabora.com>
2019-01-21 19:05:47 +0000

    Copyright, SPDX and GPL headers added

--- chainloader/chainloader.h
+++ chainloader/chainloader.h
@@ -1,3 +1,23 @@
+// steamos-efi  --  SteamOS EFI Chainloader
+
+// SPDX-License-Identifier: GPL-2.0+
+// Copyright © 2018,2019 Collabora Ltd
+// Copyright © 2018,2019 Valve Corporation
+// Copyright © 2018,2019 Vivek Das Mohapatra <vivek@etla.org>
+
+// steamos-efi is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2.0 of the License, or
+// (at your option) any later version.
+
+// steamos-efi is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with steamos-efi.  If not, see <http://www.gnu.org/licenses/>.
+
 #pragma once
 
 #include "err.h"
