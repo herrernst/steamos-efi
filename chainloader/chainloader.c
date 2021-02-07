afd8ebbe466bdd9fcd0880dbe518a8149bb7e6c8
Gaël PORTAY <gael.portay@collabora.com>
2021-02-07 07:13:58 -0500

    Clear debug traces

--- chainloader/chainloader.c
+++ chainloader/chainloader.c
@@ -88,69 +88,6 @@ EFI_STATUS reboot_into_firmware (VOID)
     return EFI_SUCCESS;
 }
 
-EFI_STATUS dump_fs_details (IN EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *fs)
-{
-    EFI_STATUS res = EFI_NOT_STARTED;
-    EFI_FILE_PROTOCOL *root_dir = NULL;
-    EFI_FILE_SYSTEM_VOLUME_LABEL_INFO *volume = NULL;
-    UINTN is_esp_ish = 0;
-
-    res = efi_mount( fs, &root_dir );
-    ERROR_RETURN( res, res, L"SFSP->open-volume %x failed", (UINT64)fs );
-
-    volume = LibFileSystemVolumeLabelInfo( root_dir );
-    res = (volume ? EFI_SUCCESS : EFI_OUT_OF_RESOURCES);
-    ERROR_JUMP( res, out, L"Allocating %d bytes",
-                SIZE_OF_EFI_FILE_SYSTEM_VOLUME_LABEL_INFO + MAXFSNAMLEN );
-    v_msg( L"<<< Volume label: %s >>>\n", volume->VolumeLabel );
-
-    res = efi_file_exists( root_dir, BOOTCONFPATH );
-
-    switch( res )
-    {
-      case EFI_SUCCESS:
-        v_msg(  L"<<< !! SteamOS/bootconf, pseudo-ESP, full listing >>>\n" );
-        is_esp_ish = 1;
-        break;
-      case EFI_NOT_FOUND:
-        v_msg(  L"<<< No SteamOS/bootconf, not a pseudo-ESP >>>\n" );
-        break;
-      default:
-        WARN_STATUS( res, L"%s->Open( SteamOS/bootconf )", volume->VolumeLabel );
-    }
-
-    if( !is_esp_ish )
-        is_esp_ish = ( efi_file_exists( root_dir, EFIDIR ) == EFI_SUCCESS );
-
-    if( is_esp_ish )
-    {
-        if( efi_file_exists( root_dir, DEFAULTLDR ) == EFI_SUCCESS )
-        {
-            v_msg( L"Default loader %s exists\n", DEFAULTLDR );
-            if( valid_efi_binary( root_dir, DEFAULTLDR ) == EFI_SUCCESS )
-                v_msg( L"... and is a PE32 executable for x86_64\n" );
-            else
-                v_msg( L"... but is NOT a PE32 executable\n" );
-        }
-        else
-        {
-            v_msg( L"Default loader %s does NOT exist on this EFI volume\n",
-                   DEFAULTLDR );
-        }
-    }
-
-    if( is_esp_ish )
-        ls( root_dir, 0, L"/", 1 );
-
-    res = efi_file_close( root_dir );
-    WARN_STATUS( res, L"/->close() failed. what.\n" );
-
-out:
-    if( volume ) efi_free( volume );
-
-    return res;
-}
-
 EFI_STATUS
 EFIAPI
 efi_main (EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *sys_table)
@@ -183,9 +120,6 @@ efi_main (EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *sys_table)
 
         res = get_handle_protocol( &filesystems[ i ], &fs_guid, (VOID **)&fs );
         ERROR_CONTINUE( res, L"simple fs protocol" );
-
-        if( verbose )
-            dump_fs_details( fs );
     }
 
     res = choose_steamos_loader( filesystems, count, &steamos );
