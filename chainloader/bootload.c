89aac60da6220af226d74d942d42463aadd29abb
Vivek Das Mohapatra <vivek@collabora.com>
2021-02-15 17:54:22 +0000

    The declared struct size for UEFI structs is often a lie
    The declared layout of EFI_DEVICE_PATH_PROTOCOL is:
    
    struct
    {
     UINT8 Type;
     UINT8 SubType;
     UINT8 Length[2];
    };
    
    Or:
    
    |<-U8->|<-U8->|<-  U8×2   ->|
    [ TYPE | SUBT |  LE  | NGTH ]
    
    But the _actual_ layout is:
    |<-U8->|<-U8->|<-  U8×2   ->| LENGTH bytes here |
    [ TYPE | SUBT |  LE  | NGTH | ....              ]
    
    Fixes:
    
            chainloader/bootload.c: In function ‘choose_steamos_loader’:
            chainloader/bootload.c:166:26: warning: array subscript 2 is above array bounds of ‘UINT8[2]’ {aka ‘unsigned char[2]’} [-Warray-bounds]
              166 |             if( a->Length[ 2 + x ] != b->Length[ 2 + x ] )
                  |                 ~~~~~~~~~^~~~~~~~~
            In file included from /usr/include/efi/efi.h:44,
                             from chainloader/bootload.c:21:
            /usr/include/efi/efidevp.h:29:41: note: while referencing ‘Length’
               29 |         UINT8                           Length[2];
                  |                                         ^~~~~~
            chainloader/bootload.c:166:48: warning: array subscript 2 is above array bounds of ‘UINT8[2]’ {aka ‘unsigned char[2]’} [-Warray-bounds]
              166 |             if( a->Length[ 2 + x ] != b->Length[ 2 + x ] )
                  |                                       ~~~~~~~~~^~~~~~~~~
            In file included from /usr/include/efi/efi.h:44,
                             from chainloader/bootload.c:21:
            /usr/include/efi/efidevp.h:29:41: note: while referencing ‘Length’
               29 |         UINT8                           Length[2];
                  |                                         ^~~~~~
              GEN      steamcl.elf

--- chainloader/bootload.c
+++ chainloader/bootload.c
@@ -143,6 +143,13 @@ static UINTN swap_cfgs (found_cfg *f, UINTN a, UINTN b)
     return 1;
 }
 
+// This code compares the _medium_ part of two EFI_DEVICE_PATHS,
+// and returns TRUE if they are the same.
+//
+// It does NOT consider the filesystem-path part of the EFI_DEVICE_PATHS.
+//
+// Its main use is to determine if two files reside on the same
+// filesystem instance (ie same hardware, same partition).
 static BOOLEAN device_path_eq (EFI_DEVICE_PATH *a, EFI_DEVICE_PATH *b)
 {
     if( !a || !b )
@@ -154,16 +161,26 @@ static BOOLEAN device_path_eq (EFI_DEVICE_PATH *a, EFI_DEVICE_PATH *b)
     {
         if( DevicePathNodeLength( a ) != DevicePathNodeLength( b ) ||
             DevicePathType( a )       != DevicePathType( b )       ||
-	    DevicePathSubType( a )    != DevicePathSubType( b ) )
-	    return FALSE;
+            DevicePathSubType( a )    != DevicePathSubType( b ) )
+            return FALSE;
 
-	// both components are hard-drive partitions.
+        // both components are hard-drive partitions.
+        // if we get this far then our job is done, the two
+        // files are on the same device and partition.
         if( DevicePathType( a )    == MEDIA_DEVICE_PATH &&
-	    DevicePathSubType( a ) == MEDIA_HARDDRIVE_DP )
+            DevicePathSubType( a ) == MEDIA_HARDDRIVE_DP )
             return TRUE;
 
+        // These structs use windows style ‘put a pointer to an array
+        // at the end of a struct and deliberately overflow it’ storage.
+        // (ie allocated storage is larger than the declared struct size).
+        // See /usr/include/efi/efidevp.h
+        // As such we must trust DevicePathNodeLength and not let the
+        // compiler worry about the theoretical length of the struct
+        // because that happens to be a lie:
         for( UINT16 x = 0; x < DevicePathNodeLength( a ); x++ )
-            if( a->Length[ 2 + x ] != b->Length[ 2 + x ] )
+            if( *((UINT8 *)&(a->Length[ 0 ]) + 2 + x) !=
+                *((UINT8 *)&(b->Length[ 0 ]) + 2 + x) )
                 return FALSE;
 
         a = NextDevicePathNode( a );
